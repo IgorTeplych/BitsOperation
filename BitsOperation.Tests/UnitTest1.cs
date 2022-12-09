c13dc6a6c19deed8dd1c588f65982404d8ff510d
Igor Teplych <igorvrt@mail.ru>
2022-12-09 16:36:27 +0600

    Добавил тестовый проект

---
+++ BitsOperation.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BitsOperation.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
